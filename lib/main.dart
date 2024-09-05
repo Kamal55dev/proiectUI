53220ac6e15cf0abc469af7cec40316c350cf503
kamal55dev <kamal.arr94@gmail.com>
2024-09-05 09:37:26 +0530

    worked orientation req screen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:badminton/modules/coach_module/coach_home/views/coach_home_screen.dart';
+import 'package:badminton/modules/orientation_module/orientation_reqsuccess_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(fontFamily: "Poppins"),
+      // home: req_success(
+      //   image: 'assets/gif/req.gif',
+      //   textOne: 'Request Sent',
+      //   textTwo: 'Successfully',
+      // ),
+      home: const CoachHomeScreen(),
+    );
+  }
+}
---
+++ lib/modules/coach_module/coach_home/views/coach_home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:badminton/common/common_font_size.dart';
+import 'package:badminton/common/common_text.dart';
+import 'package:badminton/core/utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CoachHomeScreen extends ConsumerWidget {
+  const CoachHomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // var kheight = MediaQuery.of(context).size.height;
+    // var kwidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: black,
+      body: Column(
+        children: [
+          CustomText(
+            text: "text",
+            fontSize: getResponsiveFontSize(context, 12),
+          )
+        ],
+      ),
+    );
+  }
+}
