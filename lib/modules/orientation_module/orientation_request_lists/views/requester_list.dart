dfd0933bbd90a9d66f7654a27568454709d5eaad
kamal55dev <kamal.arr94@gmail.com>
2024-09-09 17:48:56 +0530

    updated

--- lib/modules/orientation_module/orientation_request_lists/views/requester_list.dart
+++ lib/modules/orientation_module/orientation_request_lists/views/requester_list.dart
@@ -72,82 +72,11 @@
 //     );
 //   }
 // }
-// import 'package:badminton/common/common_padding.dart';
-// import 'package:badminton/common/custom_appbar.dart';
-// import 'package:badminton/common/custom_tab_bar.dart';
-// import 'package:flutter/material.dart';
-// // Import the separate file where TabBar class is located
-
-// class RequesterList extends StatefulWidget {
-//   const RequesterList({super.key});
-
-//   @override
-//   State<RequesterList> createState() => _RequesterListState();
-// }
-
-// class _RequesterListState extends State<RequesterList> {
-//   @override
-//   Widget build(BuildContext context) {
-//     final size = MediaQuery.of(context).size;
-
-//     // Define the leading icon, title, and image for CustomAppbar
-//     final IconData leadingIcon = Icons.arrow_back;
-//     final String actionImage = 'assets/images/icon_images/Action Icons.png';
-
-//     return SafeArea(
-//       child: Scaffold(
-//         backgroundColor:
-//             const Color(0xFF202326), // Background color for the screen
-//         body: Column(
-//           children: [
-//             // Add CustomAppbar here
-//             Padding(
-//               padding: responsiveAllPadding(context, 0.01),
-//               child: CustomAppbar(
-//                 actionImage, // Image path for the action icon
-//                 leadingIcon: leadingIcon, // Leading icon (arrow_back)
-//                 title: 'Requesters List',
-//                 onTap: () {
-//                   // Action for leading icon tap (for example, navigate back)
-//                   Navigator.pop(context);
-//                 },
-//               ),
-//             ),
-//             // Add SizedBox between AppBar and TabBar
-
-//             // Call the CustomTabBar class
-//             const CustomTabBar(),
-
-//             Expanded(
-//               child: Stack(
-//                 children: [
-//                   Positioned.fill(
-//                     top: -100,
-//                     child: Container(
-//                       height: size.height,
-//                       width: size.width,
-//                       decoration: const BoxDecoration(
-//                         image: DecorationImage(
-//                           image: AssetImage(
-//                               'assets/images/bg_images/LoopeBGreq.png'),
-//                           alignment: Alignment.topLeft,
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:badminton/common/common_padding.dart';
 import 'package:badminton/common/custom_appbar.dart';
 import 'package:badminton/common/custom_tab_bar.dart';
 import 'package:flutter/material.dart';
+// Import the separate file where TabBar class is located
 
 class RequesterList extends StatefulWidget {
   const RequesterList({super.key});
@@ -171,7 +100,7 @@ class _RequesterListState extends State<RequesterList> {
             const Color(0xFF202326), // Background color for the screen
         body: Column(
           children: [
-            // Custom AppBar
+            // Add CustomAppbar here
             Padding(
               padding: responsiveAllPadding(context, 0.01),
               child: CustomAppbar(
@@ -184,18 +113,14 @@ class _RequesterListState extends State<RequesterList> {
                 },
               ),
             ),
+            // Add SizedBox between AppBar and TabBar
 
-            // Add SizedBox to provide space between AppBar and TabBar
-            const SizedBox(height: 16),
+            // Call the CustomTabBar class
+            const CustomTabBar(),
 
-            // Custom TabBar
-            // const CustomTabBar(),
-
-            // Expanded widget to take up the remaining space
             Expanded(
               child: Stack(
                 children: [
-                  // Positioned background image
                   Positioned.fill(
                     top: -100,
                     child: Container(
@@ -206,19 +131,8 @@ class _RequesterListState extends State<RequesterList> {
                           image: AssetImage(
                               'assets/images/bg_images/LoopeBGreq.png'),
                           alignment: Alignment.topLeft,
-                          fit: BoxFit.cover, // Ensure the image fits properly
                         ),
                       ),
-                    ),
-                  ),
-
-                  // Scrollable content goes here if necessary
-                  SingleChildScrollView(
-                    padding: responsiveAllPadding(context, 0.03),
-                    child: Column(
-                      children: [
-                        // Other widgets can go here and will be scrollable
-                      ],
                     ),
                   ),
                 ],
@@ -230,3 +144,87 @@ class _RequesterListState extends State<RequesterList> {
     );
   }
 }
+// import 'package:badminton/common/common_padding.dart';
+// import 'package:badminton/common/custom_appbar.dart';
+// import 'package:badminton/common/custom_tab_bar.dart';
+// import 'package:flutter/material.dart';
+
+// class RequesterList extends StatefulWidget {
+//   const RequesterList({super.key});
+
+//   @override
+//   State<RequesterList> createState() => _RequesterListState();
+// }
+
+// class _RequesterListState extends State<RequesterList> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final size = MediaQuery.of(context).size;
+
+//     // Define the leading icon, title, and image for CustomAppbar
+//     final IconData leadingIcon = Icons.arrow_back;
+//     final String actionImage = 'assets/images/icon_images/Action Icons.png';
+
+//     return SafeArea(
+//       child: Scaffold(
+//         backgroundColor: const Color(0xFF202326), // Background color for the screen
+//         body: Column(
+//           children: [
+//             // Custom AppBar
+//             Padding(
+//               padding: responsiveAllPadding(context, 0.01),
+//               child: CustomAppbar(
+//                 actionImage, // Image path for the action icon
+//                 leadingIcon: leadingIcon, // Leading icon (arrow_back)
+//                 title: 'Requesters List',
+//                 onTap: () {
+//                   // Action for leading icon tap (for example, navigate back)
+//                   Navigator.pop(context);
+//                 },
+//               ),
+//             ),
+
+//             // Add SizedBox to provide space between AppBar and TabBar
+//             const SizedBox(height: 16),
+
+//             // Custom TabBar
+//             const CustomTabBar(),
+
+//             // Expanded widget to take up the remaining space
+//             Expanded(
+//               child: Stack(
+//                 children: [
+//                   // Positioned background image
+//                   Positioned.fill(
+//                     top: -100,
+//                     child: Container(
+//                       height: size.height,
+//                       width: size.width,
+//                       decoration: const BoxDecoration(
+//                         image: DecorationImage(
+//                           image: AssetImage('assets/images/bg_images/LoopeBGreq.png'),
+//                           alignment: Alignment.topLeft,
+//                           fit: BoxFit.cover, // Ensure the image fits properly
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+
+//                   // Scrollable content goes here if necessary
+//                   SingleChildScrollView(
+//                     padding: responsiveAllPadding(context, 0.03),
+//                     child: Column(
+//                       children: [
+//                         // Other widgets can go here and will be scrollable
+//                       ],
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
